ce4c8c7617a48e11a16b4dbc5a57deb63f5a3395
RazerFord <desk10567@gmail.com>
2022-06-18 15:35:45 +0700

    delete category

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,7 +4,7 @@ return [
     'created' => 'entity created',
     'updated' => 'entity updated',
     'deleted' => 'entity deleted',
-    'has_product' => 'has product',
+    'has_product' => 'the category is attached to the product',
     'validation' => [
         'error' => 'error validation'
     ]
